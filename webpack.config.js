d0c14db80baf96b4dff5f1547838327c9e5bcb1a
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-12-18 14:47:48 -0400

    Add webpack dev server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
   module: {
     rules: [
       {
