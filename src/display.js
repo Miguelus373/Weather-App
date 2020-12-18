943044645a3380d2c50152b7e9379128180d60b2
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-12-18 12:29:01 -0400

    Add error handling

--- src/display.js
+++ src/display.js
@@ -2,16 +2,17 @@ import toggleUnit from './toggle_unit';
 
 const displayWeather = async (weatherData) => {
   const data = await weatherData;
-  const {
-    name: city, sys: { country },
-    weather: [{ description, icon }],
-    main: {
-      temp: temperature, humidity, pressure, feels_like: feelsLike,
-    },
-    wind: { speed },
-  } = data;
+  if (data && data.cod < 400) {
+    const {
+      name: city, sys: { country },
+      weather: [{ description, icon }],
+      main: {
+        temp: temperature, humidity, pressure, feels_like: feelsLike,
+      },
+      wind: { speed },
+    } = data;
 
-  document.querySelector('.weather-box').innerHTML = `
+    document.querySelector('.weather-box').innerHTML = `
     <h1>${city}, <span class="country">${country}</span></h1>
     <div class="weather-header">
     <img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>
@@ -19,14 +20,15 @@ const displayWeather = async (weatherData) => {
     <p class="temp">${temperature}°</p>
     <p class="toggle"><a href="#" id="toggle">Change to Fahrenheit</a></p>
       <div class="weather-info">
-        <p class="info-item">Wind <br/> ${speed} m/s</p>
+      <p class="info-item">Wind <br/> ${speed} m/s</p>
         <p class="info-item">Feels like <br/> <span id="feels-like">${feelsLike}°</span></p>
         <p class="info-item">Humidity <br/> ${humidity}%</p>
         <p class="info-item">Pressure <br/> ${pressure} hPa</p>
-      </div>
-    </div>
-    `;
-  toggleUnit();
+        </div>
+        </div>
+        `;
+    toggleUnit();
+  }
 };
 
 export { displayWeather as default };
--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,11 @@
 const getWeather = async (url) => {
-  const currentWeather = await fetch(url);
-  const data = await currentWeather.json();
-
-  return data;
+  try {
+    const currentWeather = await fetch(url);
+    const data = await currentWeather.json();
+    return data;
+  } catch (e) {
+    return false;
+  }
 };
 
 export { getWeather as default };
